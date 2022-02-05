11294fa5a80d7ab77be53026344c26f468e0d70d
Ryan Leake <leakey94@gmail.com>
2022-02-05 12:35:42 +0000

    Create project

---
+++ Shortcuts/ShortcutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutsApp.swift
+//  Shortcuts
+//
+//  Created by Ryan Leake on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ShortcutsView()
+        }
+    }
+}
---
+++ ShortcutsTests/ShortcutsTests.swift
@@ -0,0 +1,31 @@
+//
+//  ShortcutsTests.swift
+//  ShortcutsTests
+//
+//  Created by Ryan Leake on 05/02/2022.
+//
+
+import XCTest
+@testable import Shortcuts
+
+class ShortcutsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
